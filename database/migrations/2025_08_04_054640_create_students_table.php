5c678595386308dee55b24e6c625e6e1d62f4777
Ruvindu <ruvindusharada22@gmail.com>
2025-08-04 11:27:17 +0530

    Implement Student resource with CRUD functionality and migration updates

--- database/migrations/2025_08_04_054640_create_students_table.php
+++ database/migrations/2025_08_04_054640_create_students_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+             $table->string('name');
+            $table->string('email')->unique();
+            $table->string('image')->nullable();
+            $table->string('password');
         });
     }
 
